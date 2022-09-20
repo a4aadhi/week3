eba7d81cc5128b5bfa34cedd35cfc4f4e620623e
Adarshmnair1999 <a4aadarsh1999@gmail.com>
2022-09-20 19:14:12 -0400

    first commit

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 console.log("week3");
+const exp = require("express")
+const app =exp();
+
+app.get("/",function(req,res){
+    res.send("home page")
+
+});
+app.get("/about_us",function(){
+    res.send("about us page")
+
+});
+app.post("/login",function(req,res){
+    res.send("get:")
+
+});
+var port = process.env.PORT || 8000;
+
+app.listen(port);
